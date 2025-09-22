0f5e6c9d4b1a3eec6630b4c2291a74745dec1e67
Alexandre Borba <alexandre.borba@gmail.com>
2025-09-22 17:08:26 +0100

    Update 22-09-2025 - 17:08

--- www/res/js/_config.js
+++ www/res/js/_config.js
@@ -2,9 +2,9 @@
   window.APP_CONFIG = {
     shipCode:  'AMA',
     // server:    'http://10.176.68.119',
-    // server:    'http://10.160.3.1', // srvdockerlab
+    server:    'http://10.160.3.1', // srvdockerlab
     // server:    'http://srvdockerlab',
-    server:    'http://iptv.ama.local',
-    container: 'mrc'
+    //server:    'http://iptv.ama.local',
+    container: 'mrc-ama'
   };
 })(window);
